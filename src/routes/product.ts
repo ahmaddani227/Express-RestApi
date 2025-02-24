0d1e3f423fc7ef9d436ebf6233886d21c0a3e64d
Ahmad Dani <putraahmaddani833@gmail.com>
2025-02-24 13:11:00 +0700

    feat: logger pino and handle request

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,13 +1,22 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { logger } from "../utils/logger";
 
 export const ProductRouter: Router = Router();
 
 ProductRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res
-    .status(200)
-    .send({
-      status: true,
-      statusCode: 200,
-      data: [{ name: "sepatu compas", price: 500000 }],
-    });
+  logger.info("Product check success");
+  res.status(200).send({
+    status: true,
+    statusCode: 200,
+    data: [{ name: "sepatu compas", price: 500000 }],
+  });
+});
+
+ProductRouter.post("/", (req: Request, res: Response, next: NextFunction) => {
+  logger.info("Product check success");
+  res.status(200).send({
+    status: true,
+    statusCode: 200,
+    data: req.body,
+  });
 });
