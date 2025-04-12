144c5d08028cd7c6fbf51b66dd44cdab6b4eb543
Ahmad Dani <putraahmaddani833@gmail.com>
2025-04-12 23:38:38 +0700

    request validation with Joi

--- src/routes/product.route..ts
+++ src/routes/product.route..ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { logger } from "../utils/logger";
+import { createProductValidation } from "../validation/product.validation";
 
 export const ProductRouter: Router = Router();
 
@@ -13,10 +14,23 @@ ProductRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
 });
 
 ProductRouter.post("/", (req: Request, res: Response, next: NextFunction) => {
+  const { error, value } = createProductValidation(req.body);
+
+  if (error) {
+    logger.error(`Error: product - create - ${error.details[0].message}`);
+    res.status(400).send({
+      status: false,
+      statusCode: 400,
+      message: error.details[0].message,
+      data: {},
+    });
+  }
+
   logger.info("Product check success");
   res.status(200).send({
     status: true,
     statusCode: 200,
+    message: "Product created successfully",
     data: req.body,
   });
 });
